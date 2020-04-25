9245f09aaa656eefae76d248b00c0f52f27eed25
mkbeh <mkbehforever@gmail.com>
2020-04-26 00:28:57 +0300

    Added basics

--- __main__.py
+++ __main__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import re
+
 import telebot
 from telebot import types
 
@@ -12,23 +14,14 @@ bot = telebot.TeleBot(API_TOKEN)
 user_dict = {}
 
 
-class ValueChecker:
-    @staticmethod
-    def is_available_value(value, iterable):
-        if value in iterable:
-            return True
-
-        return False
-
-
 class GirlsFilter:
     pass
 
 
 class User:
-    def __init__(self, country, town):
-        self.city = country
-        self.town = town
+    def __init__(self):
+        self.city = None
+        self.town = None
 
 
 def create_reply_keyboard(*buttons, resize_keyboard=True, row_width=2):
@@ -45,18 +38,38 @@ def create_inline_keyboard(*buttons, cb_prefix='cb_', row_width=2):
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    msg = bot.send_message(message.chat.id, WELCOME.format(message.from_user.username),
-                           parse_mode='Markdown', reply_markup=types.ReplyKeyboardRemove())
-    bot.register_next_step_handler(msg, process_country_step)
+    welcome = WELCOME.format(message.from_user.username)
+    keyboard = create_inline_keyboard(*AVAILABLE_COUNTRIES_LIST, row_width=1)
+
+    bot.send_message(message.chat.id, welcome, parse_mode='Markdown', reply_markup=types.ReplyKeyboardRemove())
+    bot.send_message(message.chat.id, 'Выберите Вашу страну из списка доступных стран', reply_markup=keyboard)
 
 
-def process_country_step(message):
-    pass
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    cb_text = call.message.json['reply_markup']['inline_keyboard'][0][0]['text']
+
+    if cb_text in AVAILABLE_COUNTRIES_LIST:
+        msg = bot.send_message(call.message.chat.id, 'Введите название города, в котором Вам нужна проститутка')
+        bot.register_next_step_handler(msg, process_city_step)
+
+
+def process_city_step(message):
+    if message.text in AVAILABLE_CITIES_LIST:
+        menu_keyboard = create_reply_keyboard(*MENU_ITEMS)
+        msg = bot.send_message(message.chat.id, MENU_WELCOME, reply_markup=menu_keyboard)
+        bot.register_next_step_handler(msg, process_menu_step)
+    elif message.text == '/reset':
+        msg = bot.send_message(message.chat.id, 'Хотите начать сначала? Введите что нибудь')
+        bot.register_next_step_handler(msg, send_welcome)
+
+    else:
+        msg = bot.send_message(message.chat.id, UNAVAILABLE_CITY)
+        bot.register_next_step_handler(msg, process_city_step)
 
 
 def process_menu_step(message):
-    menu_keyboard = create_keyboard(*MENU_ITEMS)
-    bot.send_message(message.chat.id, 'Menu text', reply_markup=menu_keyboard)
+    pass
 
 
 def main_loop():
