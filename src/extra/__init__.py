e723a4061bca2fc57e2ee793d90177d8926e9c94
mkbeh <mkbehforever@gmail.com>
2020-05-02 18:36:40 +0300

    refactored file with options

--- src/extra/__init__.py
+++ src/extra/__init__.py
@@ -8,5 +8,6 @@ API_TOKEN                       = CONFIG['api token']
 DB_ENGINE                       = CONFIG['db engine']
 ENABLE_TOR                      = CONFIG['enable tor']
 DEBUG                           = CONFIG['debug']
+
 AVAILABLE_COUNTRIES_LIST        = CONFIG['countries']
 PROMOCODES                      = utils.merge_list_of_dicts(CONFIG['promocodes'])
