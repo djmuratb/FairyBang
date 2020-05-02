ac8a9dc4423688df716bcd34eaa2f787b29cbcdc
mkbeh <mkbehforever@gmail.com>
2020-05-02 18:34:40 +0300

    Changelog: - added new option `debug` to config.yaml - refactored files with options

--- src/extra/__init__.py
+++ src/extra/__init__.py
@@ -3,9 +3,10 @@ from src.extra import utils
 
 CONFIG = utils.load_config('extra/config.yaml')
 
-ADMIN_USERNAME = CONFIG['admin username']
-API_TOKEN = CONFIG['api token']
-DB_ENGINE = CONFIG['db engine']
-ENABLE_TOR = CONFIG['enable tor']
-AVAILABLE_COUNTRIES_LIST = CONFIG['countries']
-PROMOCODES = utils.merge_list_of_dicts(CONFIG['promocodes'])
+ADMIN_USERNAME                  = CONFIG['admin username']
+API_TOKEN                       = CONFIG['api token']
+DB_ENGINE                       = CONFIG['db engine']
+ENABLE_TOR                      = CONFIG['enable tor']
+DEBUG                           = CONFIG['debug']
+AVAILABLE_COUNTRIES_LIST        = CONFIG['countries']
+PROMOCODES                      = utils.merge_list_of_dicts(CONFIG['promocodes'])
