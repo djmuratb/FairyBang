53df24212c0c1ce219ec578cd1561efc4328917c
mkbeh <mkbehforever@gmail.com>
2020-05-03 05:09:39 +0300

    Refactored common.py

--- src/models/common.py
+++ src/models/common.py
@@ -7,8 +7,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from src.extra import DATABASE
 
 
-engine = create_engine(str(URL(**DATABASE)))
-Base = declarative_base()
+url      = str(URL(**DATABASE))
+engine   = create_engine(url)
+Base     = declarative_base()
 
 class Common(Base):
     __abstract__ = True
@@ -17,7 +18,7 @@ class Common(Base):
         return {
             column.name: getattr(obj, column.key)
             for column in obj.__table__.columns
-            if column.name not in exlude_columns_names
+            if column.name not in exclude_columns_names
         }
 
 Session = sessionmaker(bind=engine)
