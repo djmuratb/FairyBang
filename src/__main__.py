f2648394a37c942fa80748fa39e18c797afcb433
mkbeh <mkbehforever@gmail.com>
2020-05-03 22:14:31 +0300

    Refactored __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -13,7 +13,7 @@ from src.extra.text import *
 
 
 if ENABLE_TOR:
-    apihelper.proxy = {'https':'socks5h://127.0.0.1:9050'}
+    apihelper.proxy = {'https': 'socks5h://127.0.0.1:9050'}
 
 bot = telebot.TeleBot(API_TOKEN)
 user_dict = {}
